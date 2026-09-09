95a7b2a51bbb363d1b5d1face83c276c66202088
Cory Schulz <coryschulz@gmail.com>
2026-09-08 23:43:33 -0500

    npm run build

--- dist/select-dropdown.esm.js
+++ dist/select-dropdown.esm.js
@@ -1157,4 +1157,3 @@ if (!customElements.get('select-label')) {
 }
 
 export { SelectDivider, SelectDropdown, SelectLabel, SelectOption, SelectPanel, SelectTrigger };
-//# sourceMappingURL=select-dropdown.esm.js.map
